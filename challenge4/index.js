aee5739255e7723e3fb40fcec7ccb141eed40f76
Devil <ddevil70707@gmail.com>
2023-01-02 15:44:50 +0545

    changed index.js

--- challenge4/index.js
+++ challenge4/index.js
@@ -7,31 +7,25 @@ for (let i = 0; i < allButtons.length; i++){
 }
 
 function changeBackground(selected_option){
-    if(selected_option.value === "red"){
-        buttonsRed();
-    }else if(selected_option.value === "orange"){
-        buttonsOrange();
-    }else if(selected_option.value === "yellow"){
-        buttonsYellow();
-    }else if(selected_option.value === "blue"){
+    if (selected_option.value === 'blue') {
         buttonsBlue();
-    }else if(selected_option.value === "random"){
+    } else if (selected_option.value === 'yellow'){
+        buttonsYellow();
+    } else if (selected_option.value === 'orange'){
+        buttonsOrange();
+    }else if (selected_option.value === 'red'){
+        buttonsRed();
+    } else if (selected_option.value === 'reset'){
+        resetButtons();
+    } else if (selected_option.value === 'random'){
         randomColors();
-    }else if(selected_option.value === "reset"){
-        resetColors();
     }
 }
 
-function buttonsRed(){
-   for (let buttonCount = 0; buttonCount < allButtons.length; buttonCount++) {
-    allButtons[buttonCount].classList.remove(allButtons[buttonCount].classList[1]);
-    allButtons[buttonCount].classList.add('red');
-   }
-}
-function buttonsOrange(){
+function buttonsBlue(){
     for (let buttonCount = 0; buttonCount < allButtons.length; buttonCount++){
         allButtons[buttonCount].classList.remove(allButtons[buttonCount].classList[1]);
-        allButtons[buttonCount].classList.add('orange');
+        allButtons[buttonCount].classList.add('blue');
     }
 }
 function buttonsYellow(){
@@ -40,29 +34,36 @@ function buttonsYellow(){
         allButtons[buttonCount].classList.add('yellow');
     }
 }
-function buttonsBlue(){
-    for (let buttonCount = 0; buttonCount < allButtons.length; buttonCount++) {
+function buttonsOrange(){
+    for (let buttonCount = 0; buttonCount < allButtons.length; buttonCount++){
         allButtons[buttonCount].classList.remove(allButtons[buttonCount].classList[1]);
-        allButtons[buttonCount].classList.add('blue');
+        allButtons[buttonCount].classList.add('orange');
     }
 }
-function resetColors(){
-    for(let buttonCount = 0; buttonCount < allButtons.length; buttonCount++){
+function buttonsRed(){
+    for (let buttonCount = 0; buttonCount < allButtons.length; buttonCount++){
+        allButtons[buttonCount].classList.remove(allButtons[buttonCount].classList[1]);
+        allButtons[buttonCount].classList.add('red');
+    }
+}
+
+function resetButtons(){
+    for (let buttonCount = 0; buttonCount < allButtons.length; buttonCount ++){
         allButtons[buttonCount].classList.remove(allButtons[buttonCount].classList[1]);
         allButtons[buttonCount].classList.add(allButtons_copy[buttonCount]);
     }
 }
 function randomColors(){
- var randomChoice = intIntoChoices(randomInt());
- console.log(randomChoice);
- for (let buttonCount = 0; buttonCount < allButtons.length; buttonCount++){
-    allButtons[buttonCount].classList.remove(allButtons[buttonCount].classList[1]);
-    allButtons[buttonCount].classList.add(randomChoice);
- }
+    for (let buttonCount  = 0; buttonCount < allButtons.length; buttonCount++){
+        allButtons[buttonCount].classList.remove(allButtons[buttonCount].classList[1]);
+        allButtons[buttonCount].classList.add(intToChoice(randomInt()));
+    }
 }
+
 function randomInt(){
-    return Math.floor(Math.random() * 3);
+    return Math.floor(Math.random() * 4);
 }
-function intIntoChoices(numbers){
-    return ['blue', 'yellow', 'orange', 'red'][numbers];
+
+function intToChoice(number){
+    return ['blue', 'yellow', 'orange', 'red'] [number];
 }
